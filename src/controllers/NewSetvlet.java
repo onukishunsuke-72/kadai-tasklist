1cb48ea72245a093fb6304278d373d18243179d8
shunsuke.onuki <onukinukishun@gmail.com>
2021-03-18 18:43:26 +0900

    Modify Index/NewServlet

--- src/controllers/NewSetvlet.java
+++ src/controllers/NewSetvlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewSetvlet
@@ -25,34 +23,18 @@ public class NewSetvlet extends HttpServlet {
      */
     public NewSetvlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
+        request.setAttribute("_token", request.getSession().getId());
 
-        //Taskのインスタンスを生成
-        Task t = new Task();
+        request.setAttribute("task", new Task());
 
-        //tの各フィールドにデータを代入
-        String content = "study";
-        t.setContent(content);
-
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-        t.setCreated_at(currentTime);
-        t.setUpdated_at(currentTime);
-
-        //データベースに保存
-        em.getTransaction().begin();
-        em.persist(t);
-        em.getTransaction().commit();
-
-        response.getWriter().append(Integer.valueOf(t.getId()).toString());
-
-        em.close();
+        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/tasks/new.jsp");
+        rd.forward(request, response);
     }
 
 }
