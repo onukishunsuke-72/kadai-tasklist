4c4cd124a7469bb4bc3303d7108815ba8524cf97
shunsuke.onuki <onukinukishun@gmail.com>
2021-03-19 16:59:15 +0900

    Modify null control

--- src/controllers/EditSetvlet.java
+++ src/controllers/EditSetvlet.java
@@ -42,7 +42,9 @@ public class EditSetvlet extends HttpServlet {
     request.setAttribute("_token",request.getSession().getId());
 
     //タスクIDをセッションスコープに登録
+    if(t != null){
     request.getSession().setAttribute("task_id", t.getId());
+    }
 
     RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/tasks/edit.jsp");
     rd.forward(request, response);
